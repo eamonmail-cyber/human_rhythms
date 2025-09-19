570f99a9a6be96485f5df61f05802a1dc8a73f00
eamonmail-cyber <eamonmail@gmail.com>
2025-09-19 20:35:33 +1000

    Update router.dart

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,9 +1,11 @@
+// lib/core/router.dart
 import 'dart:async';
 import 'package:flutter/foundation.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:go_router/go_router.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import '../features/auth/sign_in_screen.dart';
 import '../features/diary/diary_screen.dart';
 import '../features/summary/weekly_summary_screen.dart';
@@ -11,33 +13,43 @@ import '../features/routines/routine_list_screen.dart';
 import '../features/settings/settings_screen.dart';
 import '../features/auth/auth_controller.dart';
 
+/// Listenable wrapper so GoRouter refreshes when auth state changes.
 class GoRouterRefreshStream extends ChangeNotifier {
   late final StreamSubscription<dynamic> _sub;
+
   GoRouterRefreshStream(Stream<dynamic> stream) {
-      _sub =stream.asBroadcastStream().listen((_) => notifyListeners());
+    _sub = stream.asBroadcastStream().listen((_) {
+      notifyListeners();
+    });
   }
-   @override
-  void dispose();
-   _sub.cancel();
-  super.dispose();
+
+  @override
+  void dispose() {
+    _sub.cancel();
+    super.dispose();
   }
 }
-  GoRouter buildRouter() => GoRouter(
+
+/// Expose Firebase Auth state changes as a stream GoRouter can observe.
+Stream<Object?> authStateChanges() => FirebaseAuth.instance.authStateChanges();
+
+/// App router
+GoRouter buildRouter() => GoRouter(
   refreshListenable: GoRouterRefreshStream(authStateChanges()),
   routes: [
-    GoRoute(path: '/', builder: (c, s) => const AuthGate(child: DiaryScreen())),
+    GoRoute(path: '/',        builder: (c, s) => const AuthGate(child: DiaryScreen())),
     GoRoute(path: '/summary', builder: (c, s) => const AuthGate(child: WeeklySummaryScreen())),
-    GoRoute(path: '/routines', builder: (c, s) => const AuthGate(child: RoutineListScreen())),
-    GoRoute(path: '/settings', builder: (c, s) => const AuthGate(child: SettingsScreen())),
-    GoRoute(path: '/signin', builder: (c, s) => const SignInScreen()),
+    GoRoute(path: '/routines',builder: (c, s) => const AuthGate(child: RoutineListScreen())),
+    GoRoute(path: '/settings',builder: (c, s) => const AuthGate(child: SettingsScreen())),
+    GoRoute(path: '/signin',  builder: (c, s) => const SignInScreen()),
   ],
 );
 
-Stream<Object?> authStateChanges() => FirebaseAuth.instance.authStateChanges();
-
+/// Gates signed-out users to the SignIn screen.
 class AuthGate extends ConsumerWidget {
   final Widget child;
   const AuthGate({super.key, required this.child});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final auth = ref.watch(authStateProvider);
